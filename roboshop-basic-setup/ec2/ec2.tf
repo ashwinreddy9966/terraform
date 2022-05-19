f6f74a5d2d5135ae5eab24ec93a04f035abaacf0
manu <manu@local.com>
2022-05-19 12:43:05 +0530

    Auto Push Commit

--- roboshop-basic-setup/ec2/ec2.tf
+++ roboshop-basic-setup/ec2/ec2.tf
@@ -2,7 +2,8 @@ resource "aws_spot_instance_request" "cheap_worker" {
   ami                    = data.aws_ami.robot-ami.id
   instance_type          = "t3.micro"
   wait_for_fulfillment   = true
-  vpc_security_group_ids = [aws_security_group.allows.all.id]
+  vpc_security_group_ids = [aws_security_group.allows.all.id
+  ]
 
   tags = {
     Name = var.COMPONENT
