7ef0a0112fef8ab0f9c37fe0ed85cf538e300ce4
manu <manu@local.com>
2022-05-19 12:46:08 +0530

    Auto Push Commit

--- roboshop-basic-setup/ec2/ec2.tf
+++ roboshop-basic-setup/ec2/ec2.tf
@@ -3,7 +3,6 @@ resource "aws_spot_instance_request" "cheap_worker" {
   instance_type          = "t3.micro"
   wait_for_fulfillment   = true
   vpc_security_group_ids = [aws_security_group.allows.all.id]
-  ]
 
   tags = {
     Name = var.COMPONENT
@@ -22,5 +21,4 @@ resource "aws_spot_instance_request" "cheap_worker" {
             "ansible-pull -U https://github.com/ashwinreddy9966/ansible.git roboshop-pull.yml -e ENV=dev-e APP_VERSION=${APP_VERSION} -e COMPONENT=${var.COMPONENT}"
       ]
   }
-
 }
