4760590d2fccdbdf55812dd1e4911f129cbb15dc
manu <manu@local.com>
2022-05-20 11:16:57 +0530

    Auto Push Commit

---
+++ roboshop-basic-setup/vars.tf
@@ -0,0 +1,21 @@
+variable "COMPONENTS" {
+  default = [
+    "mongodb",
+    "catalogue",
+  ]
+}
+
+variable "APP_VERSION" {
+  default = [
+    "null",
+    "1.0.0"
+  ]
+}
+
+variable "ALL_COMPONENTS" {
+  default = {
+    mongodb   = "null"
+    catalogue = "1.0.0"
+    redis     = "null"
+  }
+}
