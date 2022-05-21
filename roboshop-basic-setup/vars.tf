0e19fb17629c109a65a2e178158848801bfd1c1d
manu <manu@local.com>
2022-05-21 15:25:40 +0530

    Auto Push Commit

--- roboshop-basic-setup/vars.tf
+++ roboshop-basic-setup/vars.tf
@@ -1,16 +1,16 @@
-#variable "COMPONENTS" {
-#  default = [
-#    "mongodb",
-#    "catalogue",
-#  ]
-#}
-#
-#variable "APP_VERSION" {
-#  default = [
-#    "null",
-#    "1.0.0"
-#  ]
-#}
+variable "COMPONENTS" {
+  default = [
+    "mongodb",
+    "catalogue",
+  ]
+}
+
+variable "APP_VERSION" {
+  default = [
+    "null",
+    "1.0.0"
+  ]
+}
 
 variable "ALL_COMPONENTS" {
   default = {
