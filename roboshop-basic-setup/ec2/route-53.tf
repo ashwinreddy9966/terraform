6be0c4b4b0c636047c069a0c048ff73cc4f1538f
manu <manu@local.com>
2022-05-23 12:06:01 +0530

    Auto Push Commit

--- roboshop-basic-setup/ec2/route-53.tf
+++ roboshop-basic-setup/ec2/route-53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "component" {
-  zone_id = "Z0986768G0Z9NX43SZ0Z"
-  name    = "${var.COMPONENT}-dev.robotlearning.internal"
+  zone_id = "Z0109537GJF7WTDM81TZ"
+  name    = "${var.COMPONENT}-dev.roboshop.internal"
   type    = "NS"
   ttl     = "60"
   records = [aws_spot_instance_request.cheap_worker.private_ip]
