baf3c0067e952bba3fe174bed6e0cd94cd55e988
manu <manu@local.com>
2022-05-23 12:51:27 +0530

    Auto Push Commit

--- roboshop-basic-setup/ec2/route-53.tf
+++ roboshop-basic-setup/ec2/route-53.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "component" {
   zone_id = "Z0109537GJF7WTDM81TZ"
   name    = "${var.COMPONENT}-dev.roboshop.internal"
-  type    = "NS"
+  type    = "A"
   ttl     = "60"
   records = [aws_spot_instance_request.cheap_worker.private_ip]
 
